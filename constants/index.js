8de7444898dd579b9320900020780b77eff3dbfc
fieroo <114282118+fieroo@users.noreply.github.com>
2023-08-06 13:27:31 +0430

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbd21779f79dffd13d9139f601dc2b6d76fc5b756";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
